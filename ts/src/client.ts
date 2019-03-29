af4d3f92738ad3799d508078ee4599f0ea295fc8
Amos Wenger <amoswenger@gmail.com>
2019-03-29 11:06:39 +0100

    Try calling something (it's not going great)

--- ts/src/client.ts
+++ ts/src/client.ts
@@ -1,8 +1,23 @@
 import * as capnp from "capnp-ts";
+import { ObjectSize as __O, Struct as __S } from "capnp-ts";
 import { connect } from "./connect";
 import { Message } from "capnp-ts/lib/std/rpc.capnp";
 import { Transport } from "./transport";
 import { Conn } from "./rpc";
+import { Call } from "./capability";
+import { Calculator, Calculator_Expression } from "./calculator.capnp";
+
+class Calculator_evaluate_params extends __S {
+  static readonly _capnp = {
+    displayName: "Calculator_evaluate_params",
+    id: "bb0eaae1557a36d4",
+    size: new __O(0, 1),
+  };
+
+  initExpression(): Calculator_Expression {
+    return __S.initStructAt(0, Calculator_Expression, this);
+  }
+}
 
 export async function doClient() {
   const socket = await connect("127.0.0.1:9494");
@@ -10,6 +25,24 @@ export async function doClient() {
   const conn = new Conn(transport);
   const client = await conn.bootstrap();
   console.log(`Bootstrapped! client = `, client);
+
+  const evaluateRes = await client
+    .call(<Call>{
+      method: {
+        interfaceID: capnp.Uint64.fromHexString(Calculator._capnp.id),
+        methodID: 0,
+        interfaceName: "calculator.capnp:Calculator",
+        methodName: "evaluate",
+      },
+      paramsSize: Calculator_evaluate_params._capnp.size,
+      paramsFunc: (_s: capnp.Struct) => {
+        const params = __S.getAs(Calculator_evaluate_params, _s);
+        const expr = params.initExpression();
+        expr.setLiteral(123);
+      },
+    })
+    .struct();
+  console.log(`Evaluate res = `, evaluateRes);
 
   await new Promise(resolve => setTimeout(resolve, 1000));
   process.exit(0);
