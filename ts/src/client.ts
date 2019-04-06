6fdcf33fb8955b600aca6cc112ba3bd18b342c35
Amos Wenger <amoswenger@gmail.com>
2019-04-06 22:38:02 +0200

    Use newly generated code

--- ts/src/client.ts
+++ ts/src/client.ts
@@ -19,97 +19,21 @@ import {
   Calculator_Evaluate$Results,
 } from "./calculator.capnp";
 
-class RemoteCalculator {
-  constructor(public client: Client) {}
-
-  evaluate(
-    f?: (params: Calculator_Evaluate$Params) => void,
-  ): Calculator_evaluate_Results_Promise {
-    const answer = this.client.call({
-      method: {
-        ParamsClass: Calculator_Evaluate$Params,
-        ResultsClass: Calculator_Evaluate$Results,
-        interfaceId: capnp.Uint64.fromHexString(Calculator._capnp.id),
-        methodId: 0,
-        interfaceName: "calculator.capnp:Calculator",
-        methodName: "evaluate",
-      },
-      paramsFunc: (params: Calculator_Evaluate$Params) => {
-        if (f) {
-          f(params);
-        }
-      },
-    });
-    const pipeline = new Pipeline(Calculator_Evaluate$Results, answer);
-    return new Calculator_evaluate_Results_Promise(pipeline);
-  }
-}
-
-class Calculator_evaluate_Results_Promise {
-  constructor(
-    public pipeline: Pipeline<any, any, Calculator_Evaluate$Results>,
-  ) {}
-
-  getValue(): RemoteCalculator_Value {
-    return new RemoteCalculator_Value(
-      this.pipeline.getPipeline(Calculator_Value, 0).client(),
-    );
-  }
-}
-
-class RemoteCalculator_Value {
-  constructor(public client: Client) {}
-
-  read(
-    f?: (params: Calculator_Value_Read$Params) => void,
-  ): Calculator_Value_read_Promise {
-    const answer = this.client.call({
-      method: {
-        ParamsClass: Calculator_Value_Read$Params,
-        ResultsClass: Calculator_Value_Read$Results,
-        interfaceId: capnp.Uint64.fromHexString(Calculator_Value._capnp.id),
-        methodId: 0,
-        interfaceName: "calculator.capnp:Calculator.Value",
-        methodName: "read",
-      },
-      paramsFunc: (params: Calculator_Value_Read$Params) => {
-        if (f) {
-          f(params);
-        }
-      },
-    });
-    const pipeline = new Pipeline(Calculator_Value_Read$Results, answer);
-    return new Calculator_Value_read_Promise(pipeline);
-  }
-}
-
-class Calculator_Value_read_Promise {
-  constructor(
-    public pipeline: Pipeline<any, any, Calculator_Value_Read$Results>,
-  ) {}
-
-  async struct(): Promise<Calculator_Value_Read$Results | null> {
-    return await this.pipeline.struct();
-  }
-}
-
 export async function doClient() {
   const socket = await connect("127.0.0.1:9494");
   const transport = new TCPTransport(socket);
   const conn = new Conn(transport, require("weak"));
   const client = await conn.bootstrap();
-  const calc = new RemoteCalculator(client);
+  const calc = new Calculator$Client(client);
 
   async function doPipelined() {
-    const pipeline = calc
-      .evaluate(params => {
-        params.initExpression().setLiteral(123);
-      })
+    const calc = new Calculator$Client(client);
+    const req = calc
+      .evaluate(params => params.initExpression().setLiteral(456))
       .getValue()
       .read()
-      .struct();
-    const result = (await pipeline)!.getValue();
-    console.log(`(pipelined) result = ${result}`);
+      .promise();
+    console.log(`(pipelined) result = ${(await req).getValue()}`);
   }
 
   async function doNotPipelined() {
@@ -128,7 +52,7 @@ export async function doClient() {
     let b = (await a
       .getValue()
       .read()
-      .struct())!.getValue();
+      .promise()).getValue();
     console.log(`(non-pipelined) result = ${b}`);
   }
 
@@ -138,18 +62,8 @@ export async function doClient() {
     let b = (await a
       .getValue()
       .read()
-      .struct())!.getValue();
-    console.log(`(non-pipelined) result = ${b}`);
-  }
-
-  async function doGenerated() {
-    const calc = new Calculator$Client(client);
-    const req = calc
-      .evaluate(params => params.initExpression().setLiteral(456))
-      .getValue()
-      .read()
-      .promise();
-    console.log(`(generated) result = ${(await req).getValue()}`);
+      .promise()).getValue();
+    console.log(`(complex) result = ${b}`);
   }
 
   await doPipelined();
@@ -157,8 +71,6 @@ export async function doClient() {
   await doNotPipelined();
   console.log("=================");
   await doComplex();
-  console.log("=================");
-  await doGenerated();
 
   process.exit(0);
 }
